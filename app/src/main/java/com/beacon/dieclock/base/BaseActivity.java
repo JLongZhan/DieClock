4b21a5d6888fdd9fc2d00491bdcb5507364cf576
jlow <1138060860@qq.com>
2018-04-17 01:33:59 +0800

    修改沉浸式布局

--- app/src/main/java/com/beacon/dieclock/base/BaseActivity.java
+++ app/src/main/java/com/beacon/dieclock/base/BaseActivity.java
@@ -2,9 +2,11 @@ package com.beacon.dieclock.base;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import butterknife.ButterKnife;
 
@@ -18,6 +20,9 @@ public abstract class BaseActivity<T extends BasePresent> extends AppCompatActiv
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION_CODES.KITKAT <= Build.VERSION.SDK_INT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         setContentView(getResId());
         ButterKnife.bind(this);
         initView();
